4a7c789204b149498c411760237ba43fba6a5ffa
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2023-11-23 20:59:38 +0900

    feat: kotlin 공부

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,27 @@
+/*
+*
+*
+*
+*
+*
+*/
+
 fun main(args: Array<String>) {
-    val name = if(args.isEmpty()) args[0] else null
-    println("Hello, $name!")
+    for (i in 100..<200 step 2) {
+        println(i)
+    }
 }
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            e.value
+        }
+        
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
