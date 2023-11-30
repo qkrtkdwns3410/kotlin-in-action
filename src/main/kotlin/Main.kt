401945dfa1f403a3a4e76a6a85e9ac41b0fd7040
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2023-11-30 22:52:41 +0900

    feat: kotlin 공부

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,45 +5,26 @@
 *
 *
 */
-@JvmOverloads
-fun <T> joinToString(
-    collection: Collection<T>,
-    separator: String = ", ",
-    prefix: String = "",
-    postfix: String = ""
-): String {
-    val result = StringBuilder(prefix)
-    for ((index, element) in collection.withIndex()) {
-        if (index > 0) result.append(separator)
-        result.append(element)
+
+class User(val id: Long, val name: String, val age: Int, val address: String)
+
+fun User.validateBeforeSave() {
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user $id: empty $fieldName")
+        }
     }
-    result.append(postfix)
-    return result.toString()
-}
-
-val String.lastChar: Char
-    get() = get(length - 1)
-var StringBuilder.lastChar: Char
-    get() = get(length - 1)
-    set(value: Char) {
-        this.setCharAt(length - 1, value)
-    }
-
-fun args(args: Array<String>) {
-    val argumentsList = listOf("args: ", *args)
-    println(argumentsList)
-}
-
-fun parsePath(path: String) {
-    val regex = """(.+)/(.+)\.(.+)""".toRegex()
-    val matchResult = regex.matchEntire(path)
     
-    if (matchResult != null) {
-        val (directory, filename, extension) = matchResult.destructured
-        println("Dir: $directory, name: $filename, ext: $extension")
-    }
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+fun saveUser(user: User) {
+    user.validateBeforeSave()
+    // Save user to the database
 }
 
 fun main() {
-    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+    val user = User(1, "John", 25, "Main St.")
+    saveUser(user)
 }
